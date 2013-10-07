5961e464a02a4572dcb0ec99d0f82e1cd631e3ec
David Librera <davidlibrera@gmail.com>
2013-10-07 13:01:32 +0200

    added image for a post

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -2,8 +2,19 @@ class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description
 #  validates_length_of :description, :maximum => 150
 
-  attr_accessible :title, :description, :published
+  attr_accessible :title, :description, :published, :image
 
   scope :published, lambda { where(:published => true) }
-  scope :latest, order("created_at DESC").limit(3)
+  scope :latest, order("created_at DESC").limit(2)
+
+  has_attached_file :image,
+    styles: { big: '256x256>', normal: '128x128>', mini: '32x32>' },
+    default_style: :normal,
+    url: '/spree/posts/:id/:style/:basename.:extension',
+    path: ':rails_root/public/spree/posts/:id/:style/:basename.:extension',
+    default_url: '/assets/default_post.png'
+
+  include Spree::Core::S3Support
+  supports_s3 :image
+
 end
