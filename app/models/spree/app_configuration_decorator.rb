550c4ab1879840497cfd5eaebc56626ada8af70e
David Librera <davidlibrera@gmail.com>
2013-10-09 01:38:57 +0200

    added configuration for spree banners

---
+++ app/models/spree/app_configuration_decorator.rb
@@ -0,0 +1,7 @@
+Spree::AppConfiguration.class_eval do
+  preference :news_default_url, :string, :default => '/spree/news/:id/:style/:basename.:extension'
+  preference :news_path, :string, :default => ':rails_root/public/spree/news/:id/:style/:basename.:extension'
+  preference :news_url, :string, :default => '/spree/news/:id/:style/:basename.:extension'
+  preference :news_styles, :string, :default => "{\"mini\":\"48x48>\",\"news\":\"200x200>\"}"
+  preference :news_default_style, :string, :default => 'news'
+end
--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,5 +1,7 @@
 class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description
+  validates_attachment_presence :image
+  validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif', 'image/jpg', 'image/x-png', 'image/pjpeg'], message: I18n.t(:images_only)
 #  validates_length_of :description, :maximum => 150
 
   attr_accessible :title, :description, :published, :image
@@ -8,13 +10,19 @@ class Spree::Post < ActiveRecord::Base
   scope :latest, order("created_at DESC").limit(2)
 
   has_attached_file :image,
-    styles: { news: '200x200>', mini: '32x32>' },
+    styles: { news: '200x200>', mini: '48x48>' },
     default_style: :news,
-    url: '/spree/news/:id/:style/:basename.:extension',
-    path: ':rails_root/public/spree/news/:id/:style/:basename.:extension',
-    default_url: '/assets/default_news.png'
+    url: "/spree/news/:id/:style/:basename.:extension",
+    path: ":rails_root/public/spree/news/:id/:style/:basename.:extension",
+    convert_options: { all: '-strip -auto-orient' }
 
   include Spree::Core::S3Support
   supports_s3 :image
+
+  Spree::Post.attachment_definitions[:image][:styles] = ActiveSupport::JSON.decode(Spree::Config[:news_styles]).symbolize_keys!
+  Spree::Post.attachment_definitions[:image][:path] = Spree::Config[:news_path]
+  Spree::Post.attachment_definitions[:image][:url] = Spree::Config[:news_url]
+  Spree::Post.attachment_definitions[:image][:default_url] = Spree::Config[:news_default_url]
+  Spree::Post.attachment_definitions[:image][:default_style] = Spree::Config[:news_default_style].to_sym
 
 end
